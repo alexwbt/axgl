61011bd9a97b1f955726204c25697d2ed166c44b
alexwbt <alexwbtg@gmail.com>
2024-11-20 17:44:57 +0800

    update

--- impl/opengl/include/axgl/impl/opengl/opengl_service.hpp
+++ impl/opengl/include/axgl/impl/opengl/opengl_service.hpp
@@ -67,7 +67,6 @@ public:
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
   }
 
   void terminate() override {}
