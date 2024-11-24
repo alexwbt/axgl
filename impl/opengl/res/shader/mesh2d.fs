c914fada14a1cdf79eafd87446b1f5d7002e2620
alexwbt <alexwbtg@gmail.com>
2024-11-24 19:58:20 +0800

    update

--- impl/opengl/res/shader/mesh2d.fs
+++ impl/opengl/res/shader/mesh2d.fs
@@ -1,10 +1,8 @@
 #version 330 core
 
-in vec3 vert_color;
-
 out vec4 FragColor;
 
-void main() {
+void main()
+{
   FragColor = vec4(1.0, 0.5, 0.2, 1.0);
-  // FragColor = vec4(vert_color, 1.0);
 }
