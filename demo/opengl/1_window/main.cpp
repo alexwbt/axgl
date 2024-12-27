9b787383c78ea4219aa1ba9f50086ebf999810f4
alexwbt <alexwbtg@gmail.com>
2024-12-27 22:49:21 +0800

    update

--- demo/opengl/1_window/main.cpp
+++ demo/opengl/1_window/main.cpp
@@ -1,29 +1,16 @@
-#include <iostream>
 
-#ifdef AXGL_DEBUG
+#include <axgl/axgl.hpp>
+#include <axgl/impl/glfw.hpp>
+
 int main()
 {
-  std::cout << "4 Debug" << std::endl;
+  axgl::Axgl axgl;
+
+  // register window service
+  auto window_service = axgl.use_service<axgl::impl::GlfwWindowService>();
+  // create window
+  auto window = window_service->create_window();
+  window->set_title("Hello Window!");
+
+  axgl.run();
 }
-#else
-int main()
-{
-  std::cout << "4 Release" << std::endl;
-}
-#endif
-
-// #include <axgl/axgl.hpp>
-// #include <axgl/impl/glfw.hpp>
-
-// int main()
-// {
-//   axgl::Axgl axgl;
-
-//   // register window service
-//   auto window_service = axgl.use_service<axgl::impl::GlfwWindowService>();
-//   // create window
-//   auto window = window_service->create_window();
-//   window->set_title("Hello Window!");
-
-//   axgl.run();
-// }
