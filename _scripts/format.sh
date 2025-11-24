0ebed2111dea2b4b7d836a72dd6733d7b71ab5c7
alexwbt <alexwbtg@gmail.com>
2025-11-24 22:05:41 +0800

    update format script

--- _scripts/format.sh
+++ _scripts/format.sh
@@ -5,3 +5,7 @@ ROOT_DIR="$(cd "$(dirname "$0")" && cd .. && pwd)"
 find $ROOT_DIR \
   -regex "^$ROOT_DIR/\(axgl\|demo\|impl\|lib\).*\.\(cpp\|hpp\)$" \
   -exec clang-format -i {} \;
+
+find $ROOT_DIR \
+  -regex "^$ROOT_DIR/\(axgl\|demo\|impl\|lib\).*\.\(cpp\|hpp\)$" \
+  -exec clang-tidy {} -fix -fix-errors -- -std=c++20 \;
