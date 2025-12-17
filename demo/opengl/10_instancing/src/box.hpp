ed36def7d88234fabb80a2f4e76a77dd4b5d21b8
alexwbt <alexwbtg@gmail.com>
2025-12-17 17:06:43 +0800

    update instancing demo

--- demo/opengl/10_instancing/src/box.hpp
+++ demo/opengl/10_instancing/src/box.hpp
@@ -10,6 +10,7 @@ public:
   static constexpr std::string_view kTypeId = "entity::box";
 
 private:
+  float y_ = 0.0f;
   float theta_ = 0.0f;
   float radius_ = 0.0f;
   float orbit_speed_ = 0.0f;
@@ -20,17 +21,20 @@ public:
   {
     const auto& position = transform().position;
     const glm::vec2 p2{position.x, position.z};
+    y_ = position.y;
     theta_ = std::atan2(p2.y, p2.x);
     radius_ = glm::length(p2);
   }
 
   void update(const axgl::Realm::Context& context) override
   {
+    auto& transform = this->transform();
     const auto delta_tick = static_cast<float>(context.delta_tick);
     theta_ += orbit_speed_ * delta_tick / radius_;
-    transform().position.x = std::cos(theta_) * radius_;
-    transform().position.z = std::sin(theta_) * radius_;
-    transform().rotation += rotation_speed_ * delta_tick;
+    transform.position.x = std::cos(theta_) * radius_;
+    transform.position.z = std::sin(theta_) * radius_;
+    transform.position.y = y_ + std::sin(theta_ * radius_);
+    transform.rotation += rotation_speed_ * delta_tick;
     update_model_matrix();
   }
 
