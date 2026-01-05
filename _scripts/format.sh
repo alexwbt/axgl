a14ed8e6c0c816d94577922dd3a014da42b23522
alexwbt <alexwbtg@gmail.com>
2026-01-05 13:03:47 +0800

    add pre commit hooks

--- _scripts/format.sh
+++ _scripts/format.sh
@@ -2,7 +2,7 @@
 
 ROOT_DIR="$(cd "$(dirname "$0")" && cd .. && pwd)"
 
-FILE_PATTERN="^$ROOT_DIR/\(axgl\|demo\|impl\|lib\).*\.\(cpp\|hpp\)$"
+FILE_PATTERN="^$ROOT_DIR/\(axgl\|demo\|impl\|lib\).*\\.\(cpp\|hpp\)$"
 
 if [ "$1" != "--no-tidy" ]; then
   find $ROOT_DIR \
