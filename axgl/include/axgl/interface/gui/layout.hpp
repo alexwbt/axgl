3ae00351a30355f5fa2f47eeaae132d74607b476
alexwbt <alexwbtg@gmail.com>
2026-08-08 19:38:18 +0800

    update

--- axgl/include/axgl/interface/gui/layout.hpp
+++ axgl/include/axgl/interface/gui/layout.hpp
@@ -1,21 +1,16 @@
 #pragma once
 
-#include <axgl/interface/gui/element.hpp>
-
 namespace axgl::gui
 {
+
+class Element;
 
 class Layout
 {
 public:
   virtual ~Layout() = default;
 
-  virtual glm::vec2 get_elemnt_intrinsic_size(
-    axgl::gui::Element& element, glm::vec2 available_size) const
-    = 0;
-
-  virtual void layout(axgl::gui::Element& element, glm::vec4 content_box) const
-    = 0;
+  virtual void apply(axgl::gui::Element& element) const = 0;
 };
 
 } // namespace axgl::gui
