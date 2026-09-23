2d571fcc47d1a40560da4f4e3b96df3f87c9f25e
alexwbt <alexwbtg@gmail.com>
2026-09-23 18:18:10 +0800

    adding button element impl

--- axgl/include/axgl/interface/gui/elements/button_element.hpp
+++ axgl/include/axgl/interface/gui/elements/button_element.hpp
@@ -10,10 +10,7 @@ class ButtonElement : virtual public axgl::gui::Element {
 public:
   static constexpr std::string_view kTypeId = "element:button";
 
-  virtual void set_text(const std::string& text) = 0;
-  [[nodiscard]] virtual std::string get_text() const = 0;
-
-  [[nodiscard]] virtual TextElement& label() const = 0;
+  [[nodiscard]] virtual TextElement* label() const = 0;
 
   virtual void on_click(std::function<void(const axgl::gui::Context&)> handler)
     = 0;
---
+++ impl/opengl/include/axgl/impl/opengl/gui/elements/button_element.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <axgl/common.hpp>
+#include <axgl/interface/gui/elements/button_element.hpp>
+
+#include <axgl/axgl.hpp>
+#include <axgl/impl/opengl/gui/element.hpp>
+#include <axgl/impl/opengl/gui/elements/text_element.hpp>
+
+namespace axgl::impl::opengl::gui {
+
+class ButtonElement : virtual public axgl::gui::ButtonElement,
+                      public axgl::impl::opengl::gui::Element {
+  std::function<void(const axgl::gui::Context&)> handler_;
+  std::shared_ptr<axgl::impl::opengl::gui::TextElement> label_;
+
+public:
+  void on_click(
+    std::function<void(const axgl::gui::Context&)> handler
+  ) override {
+    handler_ = std::move(handler);
+  }
+};
+
+} // namespace axgl::impl::opengl::gui
